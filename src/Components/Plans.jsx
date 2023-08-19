d819e848a9e5b790166d7e99ade68937d07c2cdb
chandra bose <chandrabosep3112@gmail.com>
2023-08-19 23:54:35 +0530

    resolving Stripe Authentication error

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useContext } from 'react';
 import { getFirestore, collection, where, query, getDocs } from 'firebase/firestore';
 import { firebaseApp } from '../firebase';
+import { userContext } from '../App'
+import { loadStripe } from '@stripe/stripe-js';
+
 
 const Plans = () => {
+  const contextUser = useContext(userContext);
   const db = getFirestore(firebaseApp);
   const [plans, setPlans] = useState([]);
 
@@ -16,15 +20,8 @@ const Plans = () => {
       for (const productDoc of querySnapshot.docs) {
         const productData = productDoc.data();
 
-        const priceSnap = await getDocs(collection(productDoc.ref, 'prices'));
-        const prices = [];
-
-        for (const priceDoc of priceSnap.docs) {
-          prices.push({
-            priceId: priceDoc.id,
-            priceData: priceDoc.data(),
-          });
-        }
+        const prices = productData.prices;
+        console.log(prices)
 
         products[productDoc.id] = {
           data: productData,
@@ -38,26 +35,51 @@ const Plans = () => {
     fetchPlans();
   }, []);
 
+ 
+
+
+  const loadCheckout = async (priceId) => {
+  
+    const docRef = await db.collection('customers')
+      .doc(contextUser.id).collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+  
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+  
+      if (error) {
+        alert(`An error occurred: ${error.message}`);
+      }
+      if (sessionId) {
+        const stripe = await loadStripe('pk_test_51LxAVjSAN2kQHJjfP1pnCuqsOb8MIwKgW1xGmYxEI8EtPA7vlG5Xl9GaHqdXIcBdLlL7Hvm7x2hPf65caWgQg1jJ00xJOgJ9ak');
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
+
+
   return (
     <div>
       {Object.entries(plans).map(([id, e]) => {
         return (
-          <div key={id}>
+          <div key={id} className='flex items-center justify-between'>
             {e.data && (
-              <div>
-                <h5>{e.data.name}</h5>
-                <h6>{e.data.description}</h6>
-                <ul>
-                  {e.prices.map(price => (
-                    <li key={price.priceId}>
-                      {price.priceData.currency} {price.priceData.unit_amount / 100} per {price.priceData.interval}
-                    </li>
-                  ))}
-                </ul>
+              <div className='py-4'>
+                <h5 className='font-medium'>{e.data.name}</h5>
+                <h6 className='font-base text-sm'>{e.data.description}</h6>
               </div>
             )}
-            <button>Subscribe</button>
-          </div>
+              <button
+                onClick={() => loadCheckout(e.prices[0].id)}
+                className='py-1 md:my-6 px-[4%] rounded-sm cursor-pointer bg-[#e50914f3]'
+              >
+                Subscribe
+              </button>         
+            </div>
         );
       })}
     </div>
